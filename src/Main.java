5cbf01db97435396ab9e135b7d4be891842f346c
Shuitirou Sakihama <e195706@ie.u-ryukyu.ac.jp>
2019-11-28 09:56:55 +0900

    HW4 Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        String str;
-        str = null;
+       try {
+           String str;
+           str = null;
 
-        System.out.println(str.length());
+           System.out.println(str.length());
+       }catch(NullPointerException e){
+           System.out.println("例外が発生中！");
+           System.out.println(e.getMessage());
+        }
     }
 }
